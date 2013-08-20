c6a9293431814cb01f5db3ece94d7a9a3a99318b
Kevin Credo <krcredo@gmail.com>
2013-08-20 16:07:49 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenwhale::Application.config.secret_token = '73744b252e4f60859c14fa5e6d19e6606cfc8d75612537ab8d8484aa59772511050a2ece935fdbda67ee7b81aaf06a5a664d8a9ea60d28f6c6f4cd64282f60c3'
